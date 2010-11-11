158d9d0d0eafb72e1c474b898ac2c90477df405e
Craig Russell <craig@craig-russell.co.uk>
2010-11-11 13:03:47 +0000

    Added LDAPService method outlines

--- lib/LDAPService.php
+++ lib/LDAPService.php
@@ -20,40 +20,72 @@ class LDAPService {
     /*
     *   Define your LDAP server connection information here
     *
-    *   server      Array of LDAP server IPs or hostnames
-    *   binduser    DN of user for LDAP bind
-    *   bindpass    Password of bind user
-    *   bindbase    Restrict activity to subtree of this container
+    *   ldap_server      LDAP server pool IP or domain name
+    *   ldap_binduser    DN of user for LDAP bind
+    *   ldap_bindpass    Password of bind user
+    *   ldap_bindbase    Restrict activity to subtree of this container
     */
-    private $_info['server']        = array('a.a.a.a','b.b.b.b','c.c.c.c');
-    private $_info['binduser']      = 'cn=admin,o=org';
-    private $_info['bindpass']      = 'passw0rd';
-    private $_info['bindbase']      = 'ou=people,o=org';
+    const ldap_server        = 'x.x.x.x';
+    const ldap_binduser      = 'cn=admin,o=org';
+    const ldap_bindpass      = 'passw0rd';
+    const ldap_bindbase      = 'ou=people,o=org';
+
+    /*
+    *   DO NOT MAKE ANY CHANGES BELOW THIS LINE
+    */
         
     private $_conn; // Connection string
 
     /**
-    *   Define the schema for the object
+    *   Establish connection to LDAP server
     *   
-    *   @param  array   $obj_schema
-    *   @return void
     */
     public function __construct()
     {
     	// Connect to LDAP server
     }
 
+    /**
+    *   Break connection to LDAP server
+    *   
+    */
 	public function __destruct()
     {
         // Disconnect from LDAP Server
     }
 
-    public function search()
+    /**
+    *   Search LDAP directory with $search
+    *   
+    *   @param  string   $search
+    *   @return array
+    */
+    public function search($search)
     {
-        return $this;
+        return $search;
     }
 
-    public function save()
+    /**
+    *   Save $data to LDAP directory
+    *   Update object if it exists
+    *   Attempt to create new object if it does not
+    *   
+    *   @param  array   $data
+    *   @return bool
+    */
+    public function save($data)
+    {
+        return true;
+    }
+    
+    /**
+    *   Attempt to authenticate against LDAP server
+    *   
+    *   @param  string   $username
+    *   @param  string   $password
+    *   @return bool
+    */
+    public function authenticate($username, $password)
     {
         return true;
     }
